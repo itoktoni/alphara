1eec7a6d4be1bee772d3dbdb450450a94fe1e004
itoktoni <itok.toni@gmail.com>
2023-12-20 07:16:24 +0700

    update

--- public/ip.php
+++ public/ip.php
@@ -1,6 +1,6 @@
 <?php
 
 header('Content-Type: application/json; charset=utf-8');
-$data = ['domain' => 'https://obsesiman.online'];
+$data = ['domain' => 'https://obsesiman.my.id'];
 
 echo json_encode($data);
